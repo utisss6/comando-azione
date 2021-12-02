cbd656856597518e9e9d431ec23ec7fe16d92269
utisss.#0672 <90255812+utisss6@users.noreply.github.com>
2021-12-02 19:16:57 +0100

    Update client.lua

--- utis_azione/client.lua
+++ utis_azione/client.lua
@@ -8,16 +8,11 @@ end)
 
 RegisterNetEvent('utisss:azione')
 AddEventHandler('utisss:azione', function(id, name, message, time)
-    local inviatore = PlayerId()
-    local ricevitori = GetPlayerFromServerId(id) 
-    if ricevitori == inviatore then
+    local id1 = PlayerId()
+    local id2 = GetPlayerFromServerId(id) 
+if GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(id1)), GetEntityCoords(GetPlayerPed(id2)), false) < 200 then
         TriggerEvent('chat:addMessage', {
-			template = '<div class="chat-message ooc"><i class="fas fa-door-open"></i> <b><span style="color: ##FF0000">[AZIONE] {0}</span>&nbsp;<span style="font-size: 14px; color: #e1e1e1;">{2}</span></b><div style="margin-top: 5px; font-weight: 300;">{1}</div></div>',
-			args = { name, message, time }
-		})
-    elseif GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(inviatore)), GetEntityCoords(GetPlayerPed(ricevitori)), true) < 200.0 then
-        TriggerEvent('chat:addMessage', {
-			template = '<div class="chat-message ooc"><i class="fas fa-door-open"></i> <b><span style="color: ##FF0000">[AZIONE] {0}</span>&nbsp;<span style="font-size: 14px; color: #e1e1e1;">{2}</span></b><div style="margin-top: 5px; font-weight: 300;">{1}</div></div>',
+			template = '<div class="chat-message"><i class="fas fa-door-open"></i> <b><span style="color: ##FF0000">[AZIONE] {0}</span>&nbsp;<span style="font-size: 14px; color: #e1e1e1;">{2}</span></b><div style="margin-top: 5px; font-weight: 300;">{1}</div></div>',
 			args = { name, message, time }
 		})
     end
