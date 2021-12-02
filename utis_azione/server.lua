596680cbce070c6d9bcca37b051dd32496ccf69c
utisss.#0672 <90255812+utisss6@users.noreply.github.com>
2021-12-02 19:18:08 +0100

    Update server.lua

--- utis_azione/server.lua
+++ utis_azione/server.lua
@@ -5,7 +5,7 @@ RegisterCommand('azione', function(source, args, rawCommand)
 	local xPlayer = ESX.GetPlayerFromId(source)
 	local message = rawCommand:sub(7)
 	playerName = xPlayer.getName()
-	TriggerClientEvent('utisss:azione', -1,source, GetPlayerName(source), message, '')
+	TriggerClientEvent('utisss:azione', -1,source, GetPlayerName(source)..' ['..source..']:', message, '')
 end)
 
 --utisss.#0672
